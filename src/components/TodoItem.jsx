cd7e8a85211b5a28308d2498ed4c216ebe9a6e76
peter <peterling082388283@gmail.com>
2025-11-12 19:33:47 +0800

    hiiiii

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,4 @@
-function Task(props) {
-  const name = props.name;
+function Task(name) {
   return (
     <div className="d-flex justify-content-between align-items-center mt-3">
       <h5 className="mb-0">1. {name}</h5>
